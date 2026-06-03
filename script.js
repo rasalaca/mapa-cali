d9a59c7060a5daa872e86d68009cbd44152db618
rasalaca <raul.lara@correounivalle.edu.co>
2026-06-02 19:06:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,77 @@
+const map = L.map("map").setView([3.4516, -76.5320], 12);
 
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: "© OpenStreetMap"
+}).addTo(map);
+
+function obtenerNombreBarrio(feature) {
+  return feature.properties.barrio ||
+         feature.properties.BARRIO ||
+         feature.properties.nombre ||
+         feature.properties.NOMBRE ||
+         feature.properties.id_barrio ||
+         feature.properties.ID_BARRIO ||
+         "Sin nombre";
+}
+
+function obtenerComuna(feature) {
+  return feature.properties.comuna ||
+         feature.properties.COMUNA ||
+         feature.properties.cod_comuna ||
+         feature.properties.COD_COMUNA ||
+         feature.properties.id_comuna ||
+         feature.properties.ID_COMUNA ||
+         "Sin dato";
+}
+
+let capaBarrios;
+
+fetch("BarriosCali.geojson")
+  .then(response => response.json())
+  .then(barrios => {
+    capaBarrios = L.geoJSON(barrios, {
+      style: {
+        color: "#555555",
+        weight: 0.6,
+        fillColor: "#f97316",
+        fillOpacity: 0.35
+      },
+      onEachFeature: function(feature, layer) {
+        const barrio = obtenerNombreBarrio(feature);
+        const comuna = obtenerComuna(feature);
+
+        layer.on("click", function() {
+          document.getElementById("panel").innerHTML = `
+            <h2>${barrio}</h2>
+            <p>Comuna: ${comuna}</p>
+          `;
+        });
+
+        layer.on("mouseover", function() {
+          layer.setStyle({
+            weight: 2,
+            color: "#111111"
+          });
+        });
+
+        layer.on("mouseout", function() {
+          capaBarrios.resetStyle(layer);
+        });
+      }
+    }).addTo(map);
+
+    map.fitBounds(capaBarrios.getBounds());
+  });
+
+fetch("ComunasCali.geojson")
+  .then(response => response.json())
+  .then(comunas => {
+    L.geoJSON(comunas, {
+      style: {
+        color: "#000000",
+        weight: 2.5,
+        fillOpacity: 0
+      }
+    }).addTo(map);
+  });
